78c3b053258955cce50fdd580fb7db50be8a6208
mr cod3r <mrcod3r.ir@gmail.com>
2021-12-04 22:16:38 +0330

    until 13:00 =  add server folder

---
+++ server/api/hello.ts
@@ -0,0 +1,15 @@
+import type { IncomingMessage, ServerResponse } from "http"
+import * as url from "url"
+export default async (req: IncomingMessage, res: ServerResponse) => {
+	const queryObject = url.parse(req.url as string, true).query
+	let data = { data: [{ data: "" }] }
+
+	const { search } = queryObject
+
+	if (search) {
+		data = await $fetch(`https://www.tvmaze.com/search?q=${search}`)
+	}
+	res.writeHead(200, { "Content-Type": "application/json" })
+	res.write(JSON.stringify(data))
+	res.end()
+}
